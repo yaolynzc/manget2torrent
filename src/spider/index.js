a56f96926846a07a6333341b2bc8fc25e26afd1e
yaolynzc <yaolynzc@126.com>
2019-09-19 16:28:36 +0800

    添加dht节点

--- src/spider/index.js
+++ src/spider/index.js
@@ -6,16 +6,212 @@ const bencode = require('bencode')
 const { Table, Node } = require('./table')
 const Token = require('./token')
 
-const bootstraps = [{
-    address: 'router.bittorrent.com',
-    port: 6881
-}, {
-    address: 'dht.transmissionbt.com',
-    port: 6881
-}, {
-  address: 'router.utorrent.com',
-  port: 6881
-}]
+const bootstraps = [
+    {
+        address: 'router.bittorrent.com',
+        port: 6881
+    },
+    {
+        address: 'router.utorrent.com',
+        port: 6881
+    },
+    {
+        address: 'dht.transmissionbt.com',
+        port: 6881
+    },
+    {
+        address: 'tracker.vanitycore.co',
+        port: 6969
+    },
+    {
+        address: 'inferno.demonoid.pw',
+        port: 3418
+    },
+    {
+        address: 'open.facedatabg.net',
+        port: 6969
+    },
+    {
+        address: 'tracker.mg64.net',
+        port: 6969
+    },
+    {
+        address: '111.6.78.96',
+        port: 6969
+    },
+    {
+        address: '90.179.64.91',
+        port: 1337
+    },
+    {
+        address: '51.15.4.13',
+        port: 1337
+    },
+    {
+        address: '88.198.231.1',
+        port: 1337
+    },
+    {
+        address: '151.80.120.112',
+        port: 2710
+    },
+    {
+        address: '191.96.249.23',
+        port: 6969
+    },
+    {
+        address: '35.187.36.248',
+        port: 1337
+    },
+    {
+        address: '123.249.16.65',
+        port: 2710
+    },
+    {
+        address: '210.244.71.25',
+        port: 6969
+    },
+    {
+        address: '9.rarbg.me',
+        port: 2710
+    },
+    {
+        address: 'p4p.arenabg.com',
+        port: 1337
+    },
+    {
+        address: 'ipv4.tracker.harry.lu',
+        port: 80
+    },
+    {
+        address: 'mgtracker.org',
+        port: 2710
+    },
+    {
+        address: 'tracker.coppersurfer.tk',
+        port: 6969
+    },
+    {
+        address: 'nyaa.tracker.wf',
+        port: 7777
+    },
+    {
+        address: 'tracker.acgnx.se',
+        port: 80
+    },
+    {
+        address: 'tracker.open-internet.nl',
+        port: 6969
+    },
+    {
+        address: 'tracker.skyts.net',
+        port: 6969
+    },
+    {
+        address: 'tracker.piratepublic.com',
+        port: 1337
+    },
+    {
+        address: 'tracker.opentrackr.org',
+        port: 1337
+    },
+    {
+        address: 'allesanddro.de',
+        port: 1337
+    },
+    {
+        address: 'public.popcorn-tracker.org',
+        port: 6969
+    },
+    {
+        address: 'wambo.club',
+        port: 1337
+    },
+    {
+        address: 'tracker4.itzmx.com',
+        port: 2710
+    },
+    {
+        address: 'tracker2.christianbro.pw',
+        port: 6969
+    },
+    {
+        address: 'thetracker.org',
+        port: 80
+    },
+    {
+        address: 'tracker1.wasabii.com.tw',
+        port: 6969
+    },
+    {
+        address: 'tracker.zer0day.to',
+        port: 1337
+    },
+    {
+        address: 'tracker.xku.tv',
+        port: 6969
+    },
+    {
+        address: '62.138.0.158',
+        port: 6969
+    },
+    {
+        address: '87.233.192.220',
+        port: 6969
+    },
+    {
+        address: '78.142.19.42',
+        port: 1337
+    },
+    {
+        address: '173.254.219.72',
+        port: 6969
+    },
+    {
+        address: '51.15.76.199',
+        port: 6969
+    },
+    {
+        address: '91.212.150.191',
+        port: 3418
+    },
+    {
+        address: '103.224.212.222',
+        port: 6969
+    },
+    {
+        address: '77.91.229.218',
+        port: 6969
+    },
+    {
+        address: '5.79.83.193',
+        port: 6969
+    },
+    {
+        address: '51.15.40.114',
+        port: 80
+    },
+    {
+        address: '5.196.76.15',
+        port: 80
+    },
+    {
+        address: 'router.bitcomet.com',
+        port: 6881
+    },
+    {
+        address: 'router.magnets.im',
+        port: 6881
+    },
+    {
+        address: 'dht.aelitis.com',
+        port: 6881
+    },
+    {
+        address: 'dht.libtorrent.org',
+        port: 25401
+    }
+]
 
 function isValidPort(port) {
     return port > 0 && port < (1 << 16)
